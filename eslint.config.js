00e0f5b6d734515749119e71bcfbac208ec91076
smthbrothrjdev <99232088+smthbrothrjdev@users.noreply.github.com>
2026-02-02 12:46:42 +0100

    Fix React JSX lint rules for new runtime

--- eslint.config.js
+++ eslint.config.js
@@ -36,6 +36,8 @@ export default [
       ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       "react/prop-types": "off",
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     },
   },
